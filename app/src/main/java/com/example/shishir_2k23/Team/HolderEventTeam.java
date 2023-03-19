222c38e9b62800f50191ff60fa8f6a7b8a07e173
NorthEastKnow <madhushudhanmondal1234@gmail.com>
2023-03-19 15:43:43 +0530

    Updated Comite by madhu

--- app/src/main/java/com/example/shishir_2k23/Team/HolderEventTeam.java
+++ app/src/main/java/com/example/shishir_2k23/Team/HolderEventTeam.java
@@ -1,6 +1,7 @@
 package com.example.shishir_2k23.Team;
 
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.example.shishir_2k23.R;
 
@@ -10,9 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 public class HolderEventTeam extends RecyclerView.ViewHolder {
     TextView eventName;
     RecyclerView teamMembers;
+    ProgressBar progressBar;
     public HolderEventTeam(@NonNull View itemView) {
         super(itemView);
         eventName=itemView.findViewById(R.id.eventNameTextView);
         teamMembers=itemView.findViewById(R.id.teamMembersRecyclerView);
+        progressBar = itemView.findViewById(R.id.team_progressBar);
+
     }
 }
