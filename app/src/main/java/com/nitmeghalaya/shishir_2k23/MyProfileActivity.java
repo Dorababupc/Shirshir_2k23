9631d4052fbb2b813bd039bce5de73a08c5d7b49
NorthEastKnow <madhushudhanmondal1234@gmail.com>
2023-03-28 03:10:59 +0530

    Updated Comite by madhu

--- app/src/main/java/com/nitmeghalaya/shishir_2k23/MyProfileActivity.java
+++ app/src/main/java/com/nitmeghalaya/shishir_2k23/MyProfileActivity.java
@@ -8,25 +8,17 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -40,7 +32,7 @@ public class MyProfileActivity extends AppCompatActivity {
     private Uri mImageUri;
     private ImageButton logoutBtn;
     private ProgressBar progressBar;
-    private TextView userId;
+
     private StorageReference mStorageRef;
 
     @Override
@@ -52,11 +44,15 @@ public class MyProfileActivity extends AppCompatActivity {
         mImageView = findViewById(R.id.uploaded_image);
         logoutBtn = findViewById(R.id.log_out_id);
         progressBar = findViewById(R.id.Myprofile_progressBar);
-        userId=findViewById(R.id.userId);
-        FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
-        String userid=firebaseUser.getUid();
-        //String outerString = model.getId();
+
         mStorageRef = FirebaseStorage.getInstance().getReference("User_Uploads");
+
+        mChooseImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
 
         mUploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
